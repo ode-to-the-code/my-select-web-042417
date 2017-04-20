1c40010a58ec8350c479bf9fd846ed002bc94826
Jad Abdallah <justcallmethis@gmail.com>
2017-04-20 01:47:54 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+ new = []
+ collection.each do |thing|
+   if yield(thing) == true
+     new << thing
+   end
+  end
+  new
 end
+
+# def select
+#   [].tap do |out|
+#     each do |e| out << e if yield(e)
+#     end
+#   end
+# end
